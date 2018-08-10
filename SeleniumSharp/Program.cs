cb40a733bb30dbd35e69acaa2efff3affd68c99d
Severyn Vehera <severyn.vehera@gmail.com>
2018-08-10 22:14:50 +0300

    add project

---
+++ SeleniumSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumSharp
+{
+    class Program
+    {
+        IWebDriver driver;
+
+        static void Main(string[] args)
+        {
+         
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http:www.google.com.ua");
+        }
+
+        [Test]
+        public void TestOne()
+        {
+            IWebElement searchField = driver.FindElement(By.Name("q"));
+
+            searchField.SendKeys("Severyn Vehera");
+
+            Wait(3);
+
+            searchField.SendKeys(Keys.Enter);
+
+            Wait(3);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+                
+        public void Wait(int seconds)
+        {
+            Thread.Sleep(seconds* 1000);
+        }
+    }
+}
