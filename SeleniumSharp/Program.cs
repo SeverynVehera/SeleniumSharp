ab61d4c6b69d5761da8cd738197cd66e35a8bea6
Severyn Vehera <severyn.vehera@gmail.com>
2018-08-13 13:50:08 +0300

    Add console output

--- SeleniumSharp/Program.cs
+++ SeleniumSharp/Program.cs
@@ -22,14 +22,17 @@ namespace SeleniumSharp
         [SetUp]
         public void Initialize()
         {
+            Console.WriteLine("SetUP started");
             driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("http:www.google.com.ua");
+            Console.WriteLine("SetUP finished");
         }
 
         [Test]
         public void TestOne()
         {
+            Console.WriteLine("TestOne started");
             IWebElement searchField = driver.FindElement(By.Name("q"));
 
             searchField.SendKeys("Severyn Vehera");
@@ -39,12 +42,15 @@ namespace SeleniumSharp
             searchField.SendKeys(Keys.Enter);
 
             Wait(3);
+            Console.WriteLine("TestOne finished");
         }
 
         [TearDown]
         public void CleanUp()
         {
+            Console.WriteLine("TearDown started");
             driver.Quit();
+            Console.WriteLine("TearDown finished");
         }
                 
         public void Wait(int seconds)
